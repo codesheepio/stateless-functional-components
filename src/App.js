baa0772e49bdc778a19fe70a1d60c29579698bc6
Ake Tangkananond <iamake@gmail.com>
2017-07-04 15:22:44 +0700

    Component’s props

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ class App extends React.Component {
   render() {
     return (
       <div>
-        <Hello />
-        <Hello />
+        <Hello name="Goku" />
+        <Hello name="Gohan" />
       </div>
     );
   }
--- src/Hello.js
+++ src/Hello.js
@@ -4,7 +4,7 @@ class Hello extends React.Component {
   render() {
     return (
       <div>
-        <h1>Hello there</h1>
+        <h1>Hello {this.props.name}</h1>
         <h2>How are you doing ?</h2>
       </div>
     );
