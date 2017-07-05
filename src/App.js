ef0aa10d0ca53bba2a8cdaed91147908ad7d38c9
Ake Tangkananond <iamake@gmail.com>
2017-07-05 16:35:20 +0700

    Component’s state

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      paragraph: ''
+    };
+  }
+
   handleClick(name) {
     return () => {
-      console.log(`${name} clicked`);
+      this.setState({
+        paragraph: `${name} clicked`
+      });
     }
   }
 
   render() {
+    const { paragraph } = this.state;
+
     return (
       <div>
         <h1>React Events</h1>
+        <p>{paragraph}</p>
         <button onClick={this.handleClick('Play')}>
           Play
         </button>
