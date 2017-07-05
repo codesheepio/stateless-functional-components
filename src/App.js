4f4f7e199b6846d8c480347e5a63bc1ed9da6124
Ake Tangkananond <iamake@gmail.com>
2017-07-05 13:46:07 +0700

    Returning a function

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React from 'react';
 
 class App extends React.Component {
-  handleClick() {
-    console.log('clicked');
+  handleClick(name) {
+    return () => {
+      console.log(`${name} clicked`);
+    }
   }
 
   render() {
     return (
       <div>
         <h1>React Events</h1>
-        <button onClick={this.handleClick}>
+        <button onClick={this.handleClick('Play')}>
           Play
+        </button>
+        <button onClick={this.handleClick('Reset')}>
+          Reset
         </button>
      </div>
     );
