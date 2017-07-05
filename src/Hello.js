f51f7a35c5c77b0d26cdc567a0c9fa1f9b1b860a
Ake Tangkananond <iamake@gmail.com>
2017-07-05 10:01:52 +0700

    Functional components

--- src/Hello.js
+++ src/Hello.js
@@ -1,14 +1,12 @@
 import React from 'react';
 
-class Hello extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>Hello {this.props.name}</h1>
-        <h2>How are you doing ?</h2>
-      </div>
-    );
-  }
+function Hello(props) {
+  return (
+    <div>
+      <h1>Hello {props.name}</h1>
+      <h2>How are you doing ?</h2>
+    </div>
+  );
 }
 
 export default Hello;
