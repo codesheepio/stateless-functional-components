3221c3407afafc7313a412a89a0ef3f2ad3806b6
Ake Tangkananond <iamake@gmail.com>
2017-07-05 22:26:44 +0700

    A Clicker

--- src/App.js
+++ src/App.js
@@ -1,49 +1,14 @@
 import React from 'react';
 
-class App extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      paragraph: 'Playing',
-      playing: true,
-    };
-  }
+import Clicker from './Clicker';
 
-  handleClick(name) {
-    return () => {
-      const paragraph = `${name} clicked`;
-      switch (name) {
-        case 'Play':
-        case 'Pause':
-          this.setState({
-            paragraph,
-            playing: name === 'Play'
-          });
-          break;
-
-        case 'Reset':
-          this.setState({ paragraph });
-          break;
-      }
-    }
-  }
-
-  render() {
-    const { playing, paragraph } = this.state;
-    const play = playing ? 'Pause' : 'Play';
-    return (
-      <div>
-        <h1>React Events</h1>
-        <p>{paragraph}</p>
-        <button onClick={this.handleClick(play)}>
-          { play }
-        </button>
-        <button onClick={this.handleClick('Reset')}>
-          Reset
-        </button>
-     </div>
-    );
-  }
+function App() {
+  return (
+    <div>
+      <h1>Clicker</h1>
+      <Clicker />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/Clicker.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class Clicker extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      counter: 0
+    };
+  }
+
+  setCounter(counter) {
+    return () => this.setState({ counter });
+  }
+
+  render() {
+    const { counter } = this.state;
+
+    return (
+      <div>
+        <h3>{ counter }</h3>
+        <button onClick={this.setCounter(counter + 1)}>+</button>
+        <button onClick={this.setCounter(0)}>Reset</button>
+      </div>
+    )
+  }
+}
+
+export default Clicker;
