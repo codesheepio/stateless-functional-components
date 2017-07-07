cb43f9e6c682d8a8ccb4d6a84370d1776eb15028
Ake Tangkananond <iamake@gmail.com>
2017-07-07 17:12:51 +0700

    Reformat all buttons

--- src/Button.js
+++ src/Button.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 
 function Button(props) {
   return (
-    <button onClick={props.onClick}>
+    <button onClick={props.onClick} style={{ width: 60, color: '#00e' }}>
       { props.children }
     </button>
   )
