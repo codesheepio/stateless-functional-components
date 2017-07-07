0751a49f033355e866a63dcb5f2ed71f121666df
Ake Tangkananond <iamake@gmail.com>
2017-07-07 17:38:59 +0700

    Clicker history

--- src/Clicker.js
+++ src/Clicker.js
@@ -7,26 +7,35 @@ class Clicker extends React.Component {
   constructor() {
     super();
     this.state = {
-      counter: 0
+      counter: 0,
+      history: [],
     };
   }
 
-  setCounter(counter) {
-    return () => this.setState({ counter });
+  setCounter(counter, label) {
+    return () => this.setState({
+      counter,
+      history: counter === 0 ? [] : [...this.state.history, `${label} > ${counter}`],
+    });
   }
 
   render() {
-    const { counter } = this.state;
+    const { counter, history } = this.state;
 
     return (
       <div>
         <Counter value={ counter } />
-        <Button onClick={this.setCounter(counter + 1)}>+</Button>
-        <Button onClick={this.setCounter(counter + 7)}>+7</Button>
-        <Button onClick={this.setCounter(counter + 17)}>+17</Button>
-        <Button onClick={this.setCounter(0)}>Reset</Button>
+        <Button onClick={this.setCounter(counter + 1, '+1')}>+</Button>
+        <Button onClick={this.setCounter(counter + 7, '+7')}>+7</Button>
+        <Button onClick={this.setCounter(counter + 17, '+17')}>+17</Button>
+        <Button onClick={this.setCounter(0, 'Reset')}>Reset</Button>
+        <hr/>
+        <h4>History</h4>
+        <ul>
+          { history.map((step, i) => <li key={ i }>{ step }</li>) }
+        </ul>
       </div>
-    )
+    );
   }
 }
 
