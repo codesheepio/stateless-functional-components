ae01030d628949d2ef80bc0c9c66282e1b775682
Ake Tangkananond <iamake@gmail.com>
2017-08-01 15:28:16 +0700

    Recompose

--- src/Clicker.js
+++ src/Clicker.js
@@ -1,42 +1,43 @@
 import React from 'react';
+import { withState } from 'recompose';
 
 import Counter from './Counter';
 import Button from './Button';
 
-class Clicker extends React.Component {
-  constructor() {
-    super();
-    this.state = {
-      counter: 0,
-      history: [],
-    };
-  }
 
-  setCounter(counter, label) {
-    return () => this.setState({
-      counter,
-      history: counter === 0 ? [] : [...this.state.history, `${label} > ${counter}`],
+function Clicker({ state, setState }) {
+  const { counter, history } = state;
+  const setCounter = (nextCounter, label) => () => {
+    setState({
+      counter: nextCounter,
+      history: nextCounter === 0 ? [] : [
+        ...history,
+        `${label} > ${nextCounter}`
+      ]
     });
-  }
+  };
 
-  render() {
-    const { counter, history } = this.state;
-
-    return (
-      <div>
-        <Counter value={ counter } />
-        <Button onClick={this.setCounter(counter + 1, '+1')}>+</Button>
-        <Button onClick={this.setCounter(counter + 7, '+7')}>+7</Button>
-        <Button onClick={this.setCounter(counter + 17, '+17')}>+17</Button>
-        <Button onClick={this.setCounter(0, 'Reset')}>Reset</Button>
-        <hr/>
-        <h4>History</h4>
-        <ul>
-          { history.map((step, i) => <li key={ i }>{ step }</li>) }
-        </ul>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <Counter value={ counter } />
+      <Button onClick={
+        setCounter(counter + 1, '+1')}>+</Button>
+      <Button onClick={
+        setCounter(counter + 7, '+7')}>+7</Button>
+      <Button onClick={
+        setCounter(counter + 17,'+17')}>+17</Button>
+      <Button onClick={
+        setCounter(0, 'Reset')}>Reset</Button>
+      <hr/>
+      <h4>History</h4>
+      <ul>
+        { history.map((step, i) => <li key={ i }>{ step }</li>) }
+      </ul>
+    </div>
+  );
 }
 
-export default Clicker;
+export default withState('state', 'setState', {
+  counter: 0,
+  history: [],
+})(Clicker);
